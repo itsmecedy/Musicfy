56c854eb4061618b56a1952038778c511a23e53a
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2025-02-17 15:09:59 +0800

    remove unecessary comment and add image on browser tab

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,3 @@ function App() {
 }
 
 export default App;
-
-// echo "# Musicfy" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/itsmecedy/Musicfy.git
-// git push -u origin main
-// …or push an existing repository from the command line
-// git remote add origin https://github.com/itsmecedy/Musicfy.git
-// git branch -M main
-// git push -u origin main
